1b2642602ef4fe3a490459a2188329807e2466ed
Abdul Basit <abdul_basit313@outlook.com>
2020-08-04 04:34:23 +0500

    added context and delete feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import TaskListContextProvider from './context/TaskListContext';
 import './App.css';
 
 
-const initialTodos: Array<Todo> = [
-  { id: 1, text: 'go to gym', completed: false },
-  { id: 2, text: 'get first order on fiverr', completed: false },
-  { id: 3, text: 'finish pomodoro app', completed: true },
-  { id: 4, text: 'learn linux', completed: false },
-]
-
-
-const App = () => {
-  const [todos, setTodos] = useState(initialTodos)
-
-  const toggleTodo: toggleTodo = (selectedTodo) => {
-    const newTodos = todos.map(todo => {
-      if (todo === selectedTodo) {
-        return {
-          ...todo,
-          completed: !todo.completed
-        }
-      }
-      return todo
-    })
-    setTodos(newTodos)
-  }
-
-  const addNewTodo: addNewTodo = (newTodo) => {
-    setTodos([newTodo, ...todos])
-  }
-
-  return (
-    <>
-      <TodoForm addNewTodo={addNewTodo} />
-      <TodoList
-        todos={todos}
-        toggleTodo={toggleTodo}
-      />
-    </>
-  );
-}
+const App = () => (
+  <TaskListContextProvider>
+    <TodoForm />
+    <TodoList />
+  </TaskListContextProvider>
+)
 
 export default App;
---
+++ src/context/TaskListContext.tsx
@@ -0,0 +1,64 @@
+import React, { useState, createContext, FC, useContext } from "react"
+
+type Props = {
+  children: React.ReactNode,
+}
+
+interface TaskContextProps {
+  todos: Todo[],
+  toggleTodo: toggleTodo,
+  addNewTodo: addNewTodo,
+  deleteTodo: deleteTodo
+}
+
+const initialTodos: Array<Todo> = [
+  { id: 1, text: 'go to gym', completed: false },
+  { id: 2, text: 'get first order on fiverr', completed: false },
+  { id: 3, text: 'finish pomodoro app', completed: true },
+  { id: 4, text: 'learn linux', completed: false },
+]
+
+export const TaskListContext = createContext<TaskContextProps>({} as TaskContextProps)
+// export const TaskListContext = createContext<Partial<TaskContextProps>>({})
+// export const TaskListContext = createContext<TaskContextProps | undefined>(undefined)
+
+export const useTaskList = () => useContext(TaskListContext)
+
+const TaskListContextProvider: FC<Props> = ({ children }) => {
+  const [todos, setTodos] = useState(initialTodos)
+
+  const toggleTodo: toggleTodo = (selectedTodo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const deleteTodo: deleteTodo = (id) => {
+    let del = todos.filter((todo) => todo.id !== id)
+    setTodos(del)
+  }
+
+  const addNewTodo: addNewTodo = (newTodo) => {
+    setTodos([newTodo, ...todos])
+  }
+
+  return (
+    <TaskListContext.Provider value={{
+      todos,
+      toggleTodo,
+      deleteTodo,
+      addNewTodo
+    }}>
+      {children}
+    </TaskListContext.Provider>
+  )
+}
+
+export default TaskListContextProvider
